503a190c5af2fad305dd09fd2c6e99d466274c4c
vareversat <valentin.reversat@dev-o-sud.fr>
2020-12-28 19:55:01 +0100

    Set default value to false for checked

--- app/src/main/java/dev/vareversat/shoppl/models/Product.kt
+++ app/src/main/java/dev/vareversat/shoppl/models/Product.kt
@@ -1,6 +1,6 @@
 package dev.vareversat.shoppl.models
 
-class Product(var name: String, var quantity: Int, var unit: String, var checked: Boolean = true) {
+class Product(var name: String, var quantity: Int, var unit: String, var checked: Boolean = false) {
 
     override fun toString(): String {
         return "Product(name='$name', quantity=$quantity, unit='$unit', checked=$checked)"
